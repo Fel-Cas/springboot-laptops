383eb91986bcb44e29b88a66054c40d478dedf74
Fel-Cas <andres.cmonsalve@gmail.com>
2022-07-04 19:47:44 -0500

    Descripción de la API REST

--- src/main/java/com/store/technology/controllers/HelloController.java
+++ src/main/java/com/store/technology/controllers/HelloController.java
@@ -10,4 +10,15 @@ public class HelloController {
     public String hello(){
         return"Hola, ¿Cómo estás?";
     }
+
+    @GetMapping("/")
+    public String info(){
+        return """
+                <h1>Información de la API</h1>
+                <p>
+                Esta es una API REST hecha con spring, en la que se puede hacer un CRUD de laptops.
+                Es mi primer CRUD con Spring Boot, lo estoy aprendiendo.
+                </p>
+                """;
+    }
 }
